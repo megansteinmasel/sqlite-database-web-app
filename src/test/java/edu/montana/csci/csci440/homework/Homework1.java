4aec2907061c25fc9af0dd179de05f6cc8c03b6c
Megan Steinmasel <70349057+megansteinmasel@users.noreply.github.com>
2023-10-02 10:14:39 -0600

    Update Homework1.java

--- src/test/java/edu/montana/csci/csci440/homework/Homework1.java
+++ src/test/java/edu/montana/csci/csci440/homework/Homework1.java
@@ -14,7 +14,7 @@ public class Homework1 extends DBTest {
      * Write a query in the string below that returns all artists that have an 'A' in their name
      */
     void selectArtistsWhoseNameHasAnAInIt(){
-        List<Map<String, Object>> results = executeSQL("select * from artists where name like "A%";");
+        List<Map<String, Object>> results = executeSQL("select name from artists where name like "A%";");
         assertEquals(211, results.size());
     }
 
@@ -23,7 +23,7 @@ public class Homework1 extends DBTest {
      * Write a query in the string below that returns all artists that have more than one album
      */
     void selectAllArtistsWithMoreThanOneAlbum(){
-        List<Map<String, Object>> results = executeSQL("select artistid from albums group by artistid having count(artistid) >= 1;");
+        List<Map<String, Object>> results = executeSQL("select a.name from artists a join albums al on a.artistid = al.artistid group by a.name having count(distinct al.albumid) > 1;");
 
         assertEquals(56, results.size());
         assertEquals("AC/DC", results.get(0).get("Name"));
@@ -35,8 +35,8 @@ public class Homework1 extends DBTest {
          * album and artist name
          */
     void selectTheTrackAndAlbumAndArtistForAllTracksLongerThanSixMinutes() {
-        List<Map<String, Object>> results = executeSQL("select t.TrackId, al.Title, ar.Name from tracks t " +
-                        "inner join albums al on t.AlbumId = al.AlbumId inner join artists ar on al.ArtistId = ar.ArtistId where t.Milliseconds/6000 > 6;");
+        List<Map<String, Object>> results = executeSQL("select t.trackid, al.title, ar.name from tracks t " +
+                        "inner join albums al on t.albumid = al.albumid inner join artists ar on al.artistid = ar.artistid where t.milliseconds/6000 > 6;");
 
         assertEquals(623, results.size());
     }
