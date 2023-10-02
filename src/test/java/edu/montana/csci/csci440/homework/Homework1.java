a1fde7fd7319f00253fc69c91fb6a7f27ec38009
Megan Steinmasel <70349057+megansteinmasel@users.noreply.github.com>
2023-10-02 08:51:21 -0600

    Update Homework1.java

--- src/test/java/edu/montana/csci/csci440/homework/Homework1.java
+++ src/test/java/edu/montana/csci/csci440/homework/Homework1.java
@@ -23,8 +23,7 @@ public class Homework1 extends DBTest {
      * Write a query in the string below that returns all artists that have more than one album
      */
     void selectAllArtistsWithMoreThanOneAlbum(){
-        List<Map<String, Object>> results = executeSQL(
-                "select ArtistId from albums group by ArtistId having count(ArtistId) >= 1;");
+        List<Map<String, Object>> results = executeSQL("select artistid from albums group by artistid having count(artistid) >= 1;");
 
         assertEquals(56, results.size());
         assertEquals("AC/DC", results.get(0).get("Name"));
@@ -36,9 +35,8 @@ public class Homework1 extends DBTest {
          * album and artist name
          */
     void selectTheTrackAndAlbumAndArtistForAllTracksLongerThanSixMinutes() {
-        List<Map<String, Object>> results = executeSQL(
-                "select t.TrackId, al.Title, ar.Name from tracks t " +
-                        "inner join albums al on t.AlbumId = al.AlbumId inner join artist ar on al.ArtistId = ar.ArtistId where t.Milliseconds/6000 > 6;");
+        List<Map<String, Object>> results = executeSQL("select t.TrackId, al.Title, ar.Name from tracks t " +
+                        "inner join albums al on t.AlbumId = al.AlbumId inner join artists ar on al.ArtistId = ar.ArtistId where t.Milliseconds/6000 > 6;");
 
         assertEquals(623, results.size());
     }
